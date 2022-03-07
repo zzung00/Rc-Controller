e0ccbfdbdd428815a8b3e50e902330de3679e861
zzung00 <lee759444@gmail.com>
2022-03-07 15:37:43 +0900

    update main screen

--- Rc-Controller/View/MainView.swift
+++ Rc-Controller/View/MainView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("appleLOGO")
+                .resizable()
+                .aspectRatio(UIImage(named: "appleLOGO")!.size, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+.previewInterfaceOrientation(.landscapeRight)
     }
 }
