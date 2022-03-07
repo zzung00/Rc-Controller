be399fcd3b07345187915262ef9222efaf337360
zzung00 <lee759444@gmail.com>
2022-03-07 14:38:41 +0900

    Initial Commit

---
+++ Rc-Controller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rc-Controller
+//
+//  Created by 이정현 on 2022/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rc-Controller/Rc_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rc_ControllerApp.swift
+//  Rc-Controller
+//
+//  Created by 이정현 on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct Rc_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
